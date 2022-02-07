0f1e7011decc3946b7bf8afcf13cc1fdf70b79cd
Emanuel Garcia <93778672+Exg020@users.noreply.github.com>
2022-02-07 16:12:44 -0600

    Add files via upload

---
+++ src/static-data.js
@@ -0,0 +1,16 @@
+let items = [
+  {
+    id: 0,
+    name: "Apple Ipad",
+    description: "An Ipad like no other",
+    price: 329.0,
+  },
+  {
+    id: 1,
+    name: "Apple Ipad Pro",
+    description: "An Ipad more expensive than the ipad",
+    price: 729.0,
+  },
+];
+
+export { items };
